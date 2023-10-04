c96530222edf14155b6a36842d9da713df1cd72e
Дима <145357573+DimitryEscobarovich@users.noreply.github.com>
2023-10-04 08:58:35 +0300

    Update № 4 .py

--- Занятие 1/№ 4 .py
+++ Занятие 1/№ 4 .py
@@ -1,6 +1,3 @@
-seconds = 345600
-x = int(seconds/86400)
-y = int(seconds/3600)
-z = int(seconds/60)
-a = seconds
-print (x, y, z, a)
+import datetime
+s = 354520
+print(datetime.timedelta(seconds=s))
